ce4085416a07508ac5d9ea954a0b9c698326b69f
wzn <13368343973@163.com>
2024-11-20 18:42:25 +0800

    fix: 更新用户信息

--- packages/web-components/src/creek-layout/HeaderContent/UserInfo.tsx
+++ packages/web-components/src/creek-layout/HeaderContent/UserInfo.tsx
@@ -1,20 +1,54 @@
-import { Avatar, Space } from "antd";
+import { LogoutOutlined } from "@ant-design/icons";
+import { Avatar, Dropdown, DropDownProps, Space } from "antd";
 import { createStyles } from "antd-style";
 
 const useStyles = createStyles(({ token }) => ({
   avatarContainer: {
     backgroundColor: token.colorPrimary,
-    width: 25,
-    height: 25,
+    width: 24,
+    height: 24,
+  },
+  userInfoDropdownOverlay: {
+    ".ant-dropdown-menu": {
+      padding: "8px 0",
+    },
+    ".ant-dropdown-menu-item": {
+      ".ant-dropdown-menu-item-icon": {
+        fontSize: "18px",
+        marginRight: "8px",
+      },
+    },
   },
 }));
 
 export const UserInfo = () => {
   const { styles } = useStyles();
+
+  const userInfoMenu: DropDownProps["menu"] = {
+    items: [
+      {
+        key: "logout",
+        label: (
+          <Space size={8}>
+            <LogoutOutlined />
+            <span>退出登录</span>
+          </Space>
+        ),
+        onClick: () => {},
+      },
+    ],
+  };
   return (
-    <Space size={4} align="center">
-      <Avatar className={styles.avatarContainer}>C</Avatar>
-      <span>Creek</span>
-    </Space>
+    <Dropdown
+      arrow
+      placement="bottom"
+      overlayClassName={styles.userInfoDropdownOverlay}
+      menu={userInfoMenu}
+    >
+      <Space size={4} align="center">
+        <Avatar className={styles.avatarContainer}>C</Avatar>
+        <span>Creek</span>
+      </Space>
+    </Dropdown>
   );
 };
