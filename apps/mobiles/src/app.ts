15094a3bf42f3bd622bbc64be143d70e8d1734fd
wzn <13368343973@163.com>
2025-02-08 09:54:41 +0800

    fix: 新增mobile目录

---
+++ apps/mobiles/src/app.ts
@@ -0,0 +1,19 @@
+import { useDidHide, useDidShow } from '@tarojs/taro'
+import { useEffect } from 'react'
+// 全局样式
+import './app.less'
+
+function App(props) {
+  // 可以使用所有的 React Hooks
+  useEffect(() => {})
+
+  // 对应 onShow
+  useDidShow(() => {})
+
+  // 对应 onHide
+  useDidHide(() => {})
+
+  return props.children
+}
+
+export default App
