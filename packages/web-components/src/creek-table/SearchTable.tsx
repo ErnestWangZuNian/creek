0fddea637c8b83c9f9f23215e57ff0ab1b5ffa8d
wzn <13368343973@163.com>
2025-08-22 10:13:32 +0800

    更新table

--- packages/web-components/src/creek-table/SearchTable.tsx
+++ packages/web-components/src/creek-table/SearchTable.tsx
@@ -1,17 +1,32 @@
 import { ParamsType, ProTable } from '@ant-design/pro-components';
+import { createStyles } from 'antd-style';
+import classnames from 'classnames';
 import _ from 'lodash';
 import { useMemo } from 'react';
 
-import { CreekFilterDisplay } from 'dist';
-import { CreekSearchInput, useSearchContext } from '../creek-search';
+import { CreekFilterDisplay, CreekSearchInput, useSearchContext } from '../creek-search';
 import { TableViewContent } from './TableViewContent';
 import { CreekTableProps } from './type';
 
+const useStyles = createStyles(({ prefixCls }) => {
+  return {
+    'creek-table-container': {
+      [`.${prefixCls}-pagination`]: {
+        [`.${prefixCls}-pagination-total-text`]: {
+          flex: `1`,
+        },
+      },
+    },
+  };
+});
+
 // 独立的 ProTable 组件
 export const SearchProTable = <T extends ParamsType, U extends ParamsType, ValueType = 'text'>(props: CreekTableProps<T, U, ValueType>) => {
-  const { columns, params, prefixCls = 'ant', tableViewRender, pageFixedBottom, pageFixedBottomConfig, ...restProps } = props;
+  const { columns, params, prefixCls = 'ant', className, tableViewRender, pagination, pageFixedBottom = true, pageFixedBottomConfig, ...restProps } = props;
 
   const { filters, filtersToParams } = useSearchContext();
+
+  const { styles } = useStyles();
 
   // 获取搜素的参数
   const finalParams = useMemo(() => {
@@ -22,12 +37,18 @@ export const SearchProTable = <T extends ParamsType, U extends ParamsType, Value
   return (
     <ProTable<T, U, ValueType>
       {...restProps}
+      className={classnames(styles['creek-table-container'], className)}
       columns={columns}
       params={{
         ...params,
         ...finalParams,
       }}
       search={false}
+      pagination={{
+        showTotal: (total) => <span>共 {total} 条</span>,
+        showSizeChanger: true,
+        ...pagination,
+      }}
       // 在 headerTitle 中只放搜索输入框
       headerTitle={<CreekSearchInput />}
       // 在表格内容区上方显示筛选条件
