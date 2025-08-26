7454b890c0c151e0ffe13782cc91fe1514908576
wzn <13368343973@163.com>
2025-08-26 12:26:30 +0800

    style: 移除 CreekSearch 组件中的多余空行

--- packages/web-components/src/creek-search/CreekSearch.tsx
+++ packages/web-components/src/creek-search/CreekSearch.tsx
@@ -48,6 +48,7 @@ export const CreekSearch = <T extends ParamsType>(props: CreekSearchProps<T>) =>
     justify,
   });
 
+  
   return (
     <CreekSearchProvider columns={columns} onSubmit={onSubmit}>
       <div className={classnames(styles.creekSearchContainer, justify === 'end' && styles.creekSearchContainerEnd)}>
